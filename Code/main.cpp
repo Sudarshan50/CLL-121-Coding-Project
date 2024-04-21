e4812621172a0d14ad0832d507e58384843a3b3c
Sudarshan <105160284+Sudarshan50@users.noreply.github.com>
2024-04-21 13:49:58 +0000

    I/O handled

--- Code/main.cpp
+++ Code/main.cpp
@@ -8,19 +8,18 @@ const db R = 8.3144618 * 10;
 const db Pc = 220.8766; // In bar
 const db Tc = 647.096;  // In Kelvin...
 const db omega = 0.224;
-const vi ar{-7.8595178, 1.8440825, -11.786649, 22.680741, -15.9618719, 1.8012250};
-const vi secor{-0.0652869, 1.6790636 * 1e-4,40.838951, 0, 0, -3.9266518 * 1e-2, 0, 2.1157167 * 1e-2, 6.5486487 * 1e-6, 0};
-const vi thor{-1.144624 * 1e-2, 2.8274958 * 1e-5, 0, 0, 0, 1.3980876 * 1e-2, 0, -1.4349005 * 1e-2, 0, 0};
+const vi ar{-7.8595178, 1.8440825, -11.786649, 22.680741, -15.9618719, 1.8012250}; // Parameters for calculating the saturation pressure of water ....
+const vi secor{-0.0652869, 1.6790636 * 1e-4,40.838951, 0, 0, -3.9266518 * 1e-2, 0, 2.1157167 * 1e-2, 6.5486487 * 1e-6, 0}; // Paramters for calculating the activity coefficient ....
+const vi thor{-1.144624 * 1e-2, 2.8274958 * 1e-5, 0, 0, 0, 1.3980876 * 1e-2, 0, -1.4349005 * 1e-2, 0, 0}; // Paramters for calculating the activity coefficient ....
 const db sigma = 1 + sqrt(2);
 const db epsilon = 1 - sqrt(2);
 
 
-//----------------------------------------------------------------
-db spvolh2o(db temp) // Cheked...
+db spvolh2o(db temp) // Calculated the specific volume of water at a given temperature ....
 {
     return ((1 + (18.1597 * 1e-3 * temp)) / (0.9998 + (18.2249 * 1e-3 * temp) - (7.9222 * 1e-6 * pow(temp, 2)) - (55.4485 * 1e-9 * pow(temp, 3)) + (149.7562 * 1e-12 * pow(temp, 4)) - (393.2952 * 1e-15 * pow(temp, 5))));
 }
-db rho(db temp, db press) // cheked ...
+db rho(db temp, db press) //Calculating the density of water at a given temperature and pressure ....
 {
     db b = 19654.32 + 147.037 * temp - 2.2155 * pow(temp, 2) + 1.0478 * 1e-2 * pow(temp, 3) - 2.2789 * 1e-5 * pow(temp, 4);
     db a1 = 3.2891 - 2.391 * 1e-3 * temp + 2.8446 * 1e-4 * pow(temp, 2) - 2.82 * 1e-6 * pow(temp, 3) + 8.477 * 1e-9 * pow(temp, 4);
@@ -31,7 +30,7 @@ db rho(db temp, db press) // cheked ...
 }
 //----------------------------------------------------------------
 
-db psh2o(db temp) // checked ....
+db psh2o(db temp) // Calculating the saturation pressure of water at a given temperature ....
 {
     db t0 = temp + 273.15;
     db gm = (1 - (t0 / Tc));
@@ -39,13 +38,13 @@ db psh2o(db temp) // checked ....
     return (exp(lterm) * Pc);
 }
 //----------------------------------------------------------------
-db fugh2o(db temp, db press)
+db fugh2o(db temp, db press) // Calculating the fugacity of water at a given temperature and pressure ....
 {
     db ps = psh2o(temp);
     return (ps * exp((18.0152 * (press - ps)) / (rho(temp, press) * R * (temp + 273.15))));
 }
-
-db kh2o(db temp, db press)
+ 
+db kh2o(db temp, db press) // Calculating the equilibrium constant of water at a given temperature and pressure ....
 {
     db fh2o = fugh2o(temp, press);
     db eqconst = -2.209+ (3.097*1e-2*temp) - (1.098*1e-4*pow(temp,2)) + (2.048*1e-7*pow(temp,3));
@@ -53,14 +52,14 @@ db kh2o(db temp, db press)
     return ((eqconst / (fh2o * press) * exp(((press - 1) * 18.18) / (R * (temp + 273.15)))));
 }
 
-db henconst(db temp, db press) // Checked.....
+db henconst(db temp, db press) // Calculating the Henry constant of water at a given temperature and pressure ....
 {
     db delb = -5.279063 + 6.187967 * pow((1e3 / (temp + 273.15)), 0.5);
     db neta = -0.114535;
     return (exp((1 - neta) * (log(fugh2o(temp, press))) + neta * (log((R * (temp + 273.15) * rho(temp, press)) / (18.18))) + (2 * rho(temp, press) * delb)));
 }
 
-vi parampure(db temp, db press)
+vi parampure(db temp, db press) // Calculating the parameters of the Peng-Robinson equation for a pure component ....
 {
     db tcritialco2 = 31+273.15;
     db pcriticalco2 = 73.8;
@@ -74,14 +73,14 @@ vi parampure(db temp, db press)
     return {a, b, m, A, B};
 }
 
-vi pengrobinson(db z, db temp, db press){
+vi pengrobinson(db z, db temp, db press){ // Calculating the Peng-Robinson equation for a given component ....
     vi param = parampure(temp,press);
     db eqn = pow(z,3)- ((1-param[4])* pow(z,2)) + (param[3]- 2*param[4]- 3*pow(param[4],2))*z - (param[3]*param[4] - pow(param[4],2) -pow(param[4],3));
     db der = 3*pow(z,2) - 2*(1-param[4])*z + (param[3]-2*param[4]-3*pow(param[4],2));
     return {eqn,der};
 }
 
-db solvepengrobinson(db zinit,db temp, db press){
+db solvepengrobinson(db zinit,db temp, db press){  //Solved the eqaution using newton rhapsn method....
     db z = -1;
     db tol = 1e-5;
     db ep = INT_MAX;
@@ -93,45 +92,14 @@ db solvepengrobinson(db zinit,db temp, db press){
     }
     return z;
 }
-db alphapr(db temp) // Checked .....
-{
-    db tcritialco2 = 31+273.15;
-    return pow((1 + (0.37464 + 1.54226 * omega - 0.26992 * pow(omega, 2)) * (1 - sqrt(temp+273.15/ tcritialco2))), 2);
-}
-vi betaqui(db temp, db press)// Checked .......
-{ 
-    db tcritialco2 = 31+273.15;
-    db pcriticalco2 = 73.8;
-    db crtemp = (temp + 273.15) / tcritialco2;
-    db beta = omega * ((press / pcriticalco2) / (crtemp));
-    db qui = (0.45724 * alphapr(crtemp)) / (omega * (crtemp));
-    return {beta, qui};
-}
 
-db pengrobison(db temp, db press) // Checked........
-{
-    vi constant = betaqui(temp, press);
-    db ep = 1e-5;
-    db zinit2 = 1;
-    db zg = -1;
-    db tol2 = INT_MAX;
-    while (tol2 > ep)
-    {
-        zg = (1 + constant[0]) - (constant[1] * constant[0] * ((zinit2 - constant[0]) / ((zinit2 + epsilon * constant[0]) * (zinit2 + sigma * constant[1]))));
-        tol2 = abs(zg - zinit2);
-        zinit2 = zg;
-    }
-    return zg;
-}
-
-
-db param(db temp, db press, vi c) // Checked................................................................
+db param(db temp, db press, vi c) //Cacluclated the parameters for the activity coefficient .....
 {
     db t0 = temp + 273.15;
     return (c[0] + (c[1] * t0) + (c[2] / t0) + (c[3] * press) + (c[4] / press) + (c[5] * (press / t0)) + (c[6] * (t0 / pow(press, 2))) + ((c[7] * press) / (630 - t0)) + (c[8] * t0 * log(press)) + (c[9] * (press / pow(t0, 2))));
 }
 
-db actcoff(db temp, db press, db molality) // Err .....
+db actcoff(db temp, db press, db molality) // Calculated the activity coefficient .....
 {
     db term1 = param(temp, press, secor); 
     db term2 = param(temp, press, thor); 
@@ -139,7 +107,7 @@ db actcoff(db temp, db press, db molality) // Err .....
     return exp(finexp);
 }
 
-db calcphi(db temp,db press)
+db calcphi(db temp,db press) // Calculated the fugacity coefficient .....
 {
     vi pm = parampure(temp, press);
     db z = solvepengrobinson(1,temp,press);
@@ -148,29 +116,22 @@ db calcphi(db temp,db press)
     return phi;
 }
 
-db kc02(db temp, db press, db molality) // Checked.....
+db kc02(db temp, db press, db molality) // Calculated the phase equilibrium constant for Co2 .....
 {
     db henry = henconst(temp, press);
     db coeff = actcoff(temp, press, molality);
-    // vi pm = parampure(temp, press);
-    // db tcritialco2 = 31+273.15;
-    // db pcriticalco2 = 73.8;
-    // db beta = 0.07779* ((press/pcriticalco2) / ((temp+273.15)/(tcritialco2)));
-    // db z = pengrobinson(temp,press);
-    // db z = pengrobison(temp,press);
-    // out<<z<<endl<<endl;
     db phi = calcphi(temp,press);
     return ((henry * coeff) / (press * phi));
 }
 
-db yh2o(db temp, db press, db molality) // Checked ......
+db yh2o(db temp, db press, db molality) // Calculated the vapour phase mole fraction for water .....
 {
     db co2 = kc02(temp, press, molality);
     db h2o = kh2o(temp, press);
     return ((1 - (1 / co2)) / ((1 / h2o) - (1 / co2)));
 }
 
-vi xco2(db temp, db press, db molality)
+vi xco2(db temp, db press, db molality) // Calculated the liquid phase mole fraction for Co2 .....
 {
     db yh20 = yh2o(temp, press, molality);
     db kco2 = kc02(temp, press, molality);
@@ -178,15 +139,6 @@ vi xco2(db temp, db press, db molality)
     return {yco2n,(yco2n / kco2)};
 }
 
-
-db zchecker(db temp, db press)
-{
-    vi param = parampure(temp, press);
-    db zl = solvepengrobinson(1, temp, press);
-    db zg = pengrobison(temp, press);
-    db check = (zg-zl) + log((zl-param[4])/(zg-param[4])) - (param[3]/(param[4]*(epsilon-sigma)))*log(((zl+sigma*param[4])*(zg+epsilon*param[4]))/((zl+epsilon*param[4])*(zg+sigma*param[4])));  
-    return (check>0)?zl:zg;
-}
 signed main()
 {
     fstream inp;
@@ -201,19 +153,22 @@ signed main()
     {
         db temp,press,molality;
         inp>>temp>>press>>molality;
-        out<<"||---------------------------------------------------------||"<<endl;
-        out<<"        -*- Temperature:- "<<temp<<"°C,"<<" "<<"Pressure:- "<<press<<"bar -*-"<<endl;
-        out<<"||---------------------------------------------------------||"<<endl;
-        out<<"Phi value:- "<<calcphi(temp,press)<<endl;
-        out<<"Henry Constant is:- "<<henconst(temp,press)<<endl;
-        out<<"Equilibrium Constant for H2o:- "<<kh2o(temp,press)<<endl;
-        out<<"Equilibrium Constant for Co2:- "<<kc02(temp,press,molality)<<endl;
-        out<<"Vapour Phase mole fraction for H2o:- "<<yh2o(temp,press,molality)<<endl;
-        out<<"Normalised vapour phase mole fraction for Co2:- "<<xco2(temp,press,molality)[0]<<endl;
-        out<<"Liquid phase mole fraction for Co2(%):- "<<xco2(temp,press,molality)[1]*100<<endl;
-        out<<"*--------------------------------------------------------------*"<<endl;
+        out<<"||----------------------------------------------------------------------||"<<endl;
+        out<<"   -*- Temperature:- "<<temp<<"K,"<<" "<<"Pressure:- "<<press<<"bar, "<<"M:- "<<molality<<"(mol/kg) -*-"<<endl;
+        out<<"||----------------------------------------------------------------------||"<<endl;
+        temp-=273.15;
+        out<<"* Phi value:- "<<calcphi(temp,press)<<endl;
+        out<<"* Henry Constant is:- "<<henconst(temp,press)<<endl;
+        out<<"* Equilibrium Constant for H2o:- "<<kh2o(temp,press)<<endl;
+        // out<<"* Equilibrium Constant for Co2:- "<<kc02(temp,press,molality)<<endl;
+        // out<<"* Activity coeff:- "<<actcoff(temp,press,molality)<<endl;
+        out<<"* Vapour Phase mole fraction for H2o:- "<<yh2o(temp,press,molality)<<endl;
+        // out<<"* Normalised vapour phase mole fraction for Co2:- "<<xco2(temp,press,molality)[0]<<endl;
+        out<<"* Liquid phase mole fraction for Co2:- "<<xco2(temp,press,molality)[1]<<endl;
+        out<<"*------------------------------------------------------------------------*"<<endl;
     }
-    out<<"*--------------------* End of the Output *------------------------*"<<endl;
+    out<<"*-------------------------* End of the Output *------------------------------*"<<endl;
+    out<<"=============================================================================="<<endl;
     
     
 }
--- Code/tempCodeRunnerFile.cpp
+++ Code/tempCodeRunnerFile.cpp
@@ -1,12 +1,2 @@
-
-db yh2o(db temp, db press, db molality) // Checked ......
-{
-    db co2 = kc02(temp, press, molality);
-    db h2o = kh2o(temp, press);
-    return ((1 - (1 / co2)) / ((1 / h2o) - (1 / co2)));
-}
-
-db xco2(db temp, db press, db molality)
-{
-    db yh20 = yh2o(temp, press, molality);
-    db kco2 = kc02(temp, press, molality);
+--------*"<<endl;
+    // cout<<"======================================================"<<
